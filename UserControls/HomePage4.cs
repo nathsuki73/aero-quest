73f0d2f75b2d3451dbe45126715d74b9d100d4f6
Neil Patrick Pajadan <152168399+Neil-Patrick@users.noreply.github.com>
2024-12-28 00:03:54 +0800

    Add HomePage4 user control to project
    Updated `aero-quest.csproj` to include the new `HomePage4` user control by adding entries for `HomePage4.cs`, `HomePage4.Designer.cs`, and `HomePage4.resx`. Created `HomePage4.cs` with a new user control class `HomePage4` inheriting from `UserControl`, including several event handler methods for button clicks. Added `HomePage4.Designer.cs` to define the layout and components of `HomePage4`, including several `Guna2ImageButton` components with their properties set and event handlers assigned.

---
+++ UserControls/HomePage4.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aero_quest.UserControls
+{
+    public partial class HomePage4 : UserControl
+    {
+        public HomePage4()
+        {
+            InitializeComponent();
+        }
+
+        private void guna2ImageButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2ImageButton2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2ImageButton6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2ImageButton7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2ImageButton3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2ImageButton5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2ImageButton4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
