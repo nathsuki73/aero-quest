2fcd4a922fadebed4ce4d29b697c124d21dc31e0
Neil Patrick Pajadan <152168399+Neil-Patrick@users.noreply.github.com>
2024-12-27 23:27:05 +0800

    Add new user control HomePage3
    The changes introduce a new user control named `HomePage3` to the project. This includes the addition of several files and modifications to the project file (`aero-quest.csproj`).
    
    In `aero-quest.csproj`:
    - Added entries for `HomePage3.cs`, `HomePage3.Designer.cs`, and `HomePage3.resx` to include them in the project and set their dependencies and subtypes appropriately.
    
    In `HomePage3.cs`:
    - Added a new class `HomePage3` which inherits from `UserControl`.
    - Implemented the constructor and event handlers for `Load`, `guna2ImageButton1_Click`, and `guna2ImageButton2_Click` events.
    
    In `HomePage3.Designer.cs`:
    - Defined the partial class `HomePage3` and its designer components.
    - Added two `Guna2ImageButton` controls (`guna2ImageButton1` and `guna2ImageButton2`) with their properties set, including event handlers for their `Click` events.
    - Set up the layout and properties of the `HomePage3` user control, including background image and size.

---
+++ UserControls/HomePage3.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace aero_quest.UserControls
+{
+    partial class HomePage3
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HomePage3));
+            this.guna2ImageButton1 = new Guna.UI2.WinForms.Guna2ImageButton();
+            this.guna2ImageButton2 = new Guna.UI2.WinForms.Guna2ImageButton();
+            this.SuspendLayout();
+            // 
+            // guna2ImageButton1
+            // 
+            this.guna2ImageButton1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("guna2ImageButton1.BackgroundImage")));
+            this.guna2ImageButton1.CheckedState.Parent = this.guna2ImageButton1;
+            this.guna2ImageButton1.HoverState.Parent = this.guna2ImageButton1;
+            this.guna2ImageButton1.Location = new System.Drawing.Point(1281, 376);
+            this.guna2ImageButton1.Name = "guna2ImageButton1";
+            this.guna2ImageButton1.PressedState.Parent = this.guna2ImageButton1;
+            this.guna2ImageButton1.Size = new System.Drawing.Size(46, 46);
+            this.guna2ImageButton1.TabIndex = 0;
+            this.guna2ImageButton1.Click += new System.EventHandler(this.guna2ImageButton1_Click);
+            // 
+            // guna2ImageButton2
+            // 
+            this.guna2ImageButton2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("guna2ImageButton2.BackgroundImage")));
+            this.guna2ImageButton2.CheckedState.Parent = this.guna2ImageButton2;
+            this.guna2ImageButton2.HoverState.Parent = this.guna2ImageButton2;
+            this.guna2ImageButton2.Location = new System.Drawing.Point(1281, 309);
+            this.guna2ImageButton2.Name = "guna2ImageButton2";
+            this.guna2ImageButton2.PressedState.Parent = this.guna2ImageButton2;
+            this.guna2ImageButton2.Size = new System.Drawing.Size(46, 46);
+            this.guna2ImageButton2.TabIndex = 1;
+            this.guna2ImageButton2.Click += new System.EventHandler(this.guna2ImageButton2_Click);
+            // 
+            // HomePage3
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.Controls.Add(this.guna2ImageButton2);
+            this.Controls.Add(this.guna2ImageButton1);
+            this.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.Name = "HomePage3";
+            this.Size = new System.Drawing.Size(1350, 729);
+            this.Load += new System.EventHandler(this.UserControl1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2ImageButton guna2ImageButton1;
+        private Guna.UI2.WinForms.Guna2ImageButton guna2ImageButton2;
+    }
+}
